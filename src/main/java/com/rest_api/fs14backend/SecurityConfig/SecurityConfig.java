69d587dcea2b90bc90cd0176a05e321465e64650
Anupa Thapa <thapaanupa@gmail.com>
2023-06-21 00:51:28 +0300

    vercel frontend url added to CORS config

--- src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/rest_api/fs14backend/SecurityConfig/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://book-management-system-rosy.vercel.app"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","https://book-management-system-88ntknuwb-anupa87.vercel.app/"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
